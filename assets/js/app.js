b5c7c2a94e39b765318d87c410eac4c326335cd1
thayna-oliveira <oliveira.thayna@outlook.com>
2019-12-02 14:55:05 -0300

    add sponsors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,11 @@
+
+
 const second = 1000,
   minute = second * 60,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date("Jan 22, 2020 00:00:00").getTime(),
+let countDown = new Date("Dec 31, 2019 00:00:00").getTime(),
   x = setInterval(function() {
     let now = new Date().getTime(),
       distance = countDown - now;
@@ -18,5 +20,5 @@ let countDown = new Date("Jan 22, 2020 00:00:00").getTime(),
       (document.getElementById("seconds").innerText = Math.floor(
         (distance % minute) / second
       ));
- 
   }, second);
+
